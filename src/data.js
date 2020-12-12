0df4b43d919ec67c68edb1864f5d163dd6726d09
Nirav Kyada <34131271+nirav-kyada@users.noreply.github.com>
2020-12-13 02:09:15 +0530

    fix - images optimized

--- src/data.js
+++ src/data.js
@@ -1,137 +1,119 @@
 export const designData = {
-  All: [
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0001",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC133V3/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/XWQ1wTW/NAJED-Round-JM-0001.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0002",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBYTXT2/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/41qvS0y/NAJED-Round-JM-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0003",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC241ZV/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/YhFTWyw/NAJED-Round-JM-0003.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0001",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBYX655/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/MD5KzQR/NAJED-MMulti-Col-0001.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0002",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC1BR66/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/BcPW5nG/NAJED-MMulti-Col-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0003",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBZSYKJ/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/t3TRCD9/NAJED-MMulti-Col-0003.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0001",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBTWRXQ/ref=twister_B08LBX8RBS?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/fdXMrHF/NAJED-0022.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0002",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0002/dp/B08KWPS1GR/",
-      image: "https://i.ibb.co/pn7pj0G/NAJED-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0003",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0023/dp/B08LBWMCJ9/",
-      image: "https://i.ibb.co/vwhDXZ0/NAJED-0023.jpg"
-    }
-  ],
-  "Round Joomars": [
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0001",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC133V3/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/XWQ1wTW/NAJED-Round-JM-0001.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0002",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBYTXT2/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/41qvS0y/NAJED-Round-JM-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Joomar - 0003",
-      subCat: "Joomar",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC241ZV/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/YhFTWyw/NAJED-Round-JM-0003.jpg"
-    }
-  ],
+	All: [
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0001',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LC133V3/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/QpDsmqM/NAJED-Round-JM-0001.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0002',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LBYTXT2/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/8m4G08q/NAJED-Round-JM-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0003',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LC241ZV/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/zWMKScf/NAJED-Round-JM-0003.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0001',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LBYX655/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/1mvm252/NAJED-MMulti-Col-0001.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0002',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LC1BR66/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/71yNWT7/NAJED-MMulti-Col-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0003',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LBZSYKJ/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/vmQZRQp/NAJED-MMulti-Col-0003.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0001',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/dp/B08LBTWRXQ/ref=twister_B08LBX8RBS?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/tBpRKFc/NAJED-0022.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0002',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0002/dp/B08KWPS1GR/',
+			image: 'https://i.ibb.co/RDqNc3w/NAJED-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0003',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0023/dp/B08LBWMCJ9/',
+			image: 'https://i.ibb.co/5YGvQqS/NAJED-0023.jpg',
+		},
+	],
+	'Round Joomars': [
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0001',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LC133V3/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/QpDsmqM/NAJED-Round-JM-0001.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0002',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LBYTXT2/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/8m4G08q/NAJED-Round-JM-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Joomar - 0003',
+			subCat: 'Joomar',
+			storeLink: 'https://www.amazon.in/dp/B08LC241ZV/ref=twister_B08LC2G2VQ?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/zWMKScf/NAJED-Round-JM-0003.jpg',
+		},
+	],
 
-  "Multi Colored Earrings": [
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0001",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBYX655/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/MD5KzQR/NAJED-MMulti-Col-0001.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0002",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LC1BR66/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/BcPW5nG/NAJED-MMulti-Col-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - MultiColor - 0003",
-      subCat: "Multi Color Earrings",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBZSYKJ/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/t3TRCD9/NAJED-MMulti-Col-0003.jpg"
-    }
-  ],
-  Execlusives: [
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0001",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/dp/B08LBTWRXQ/ref=twister_B08LBX8RBS?_encoding=UTF8&psc=1",
-      image: "https://i.ibb.co/fdXMrHF/NAJED-0022.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0002",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0002/dp/B08KWPS1GR/",
-      image: "https://i.ibb.co/pn7pj0G/NAJED-0002.jpg"
-    },
-    {
-      dName: "Natraj Art Jewellery - Execlusive - 0003",
-      subCat: "Execlusive",
-      storeLink:
-        "https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0023/dp/B08LBWMCJ9/",
-      image: "https://i.ibb.co/vwhDXZ0/NAJED-0023.jpg"
-    }
-  ]
+	'Multi Colored Earrings': [
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0001',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LBYX655/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/1mvm252/NAJED-MMulti-Col-0001.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0002',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LC1BR66/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/71yNWT7/NAJED-MMulti-Col-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - MultiColor - 0003',
+			subCat: 'Multi Color Earrings',
+			storeLink: 'https://www.amazon.in/dp/B08LBZSYKJ/ref=twister_B08LC25HVL?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/vmQZRQp/NAJED-MMulti-Col-0003.jpg',
+		},
+	],
+	Execlusives: [
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0001',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/dp/B08LBTWRXQ/ref=twister_B08LBX8RBS?_encoding=UTF8&psc=1',
+			image: 'https://i.ibb.co/tBpRKFc/NAJED-0022.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0002',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0002/dp/B08KWPS1GR/',
+			image: 'https://i.ibb.co/RDqNc3w/NAJED-0002.jpg',
+		},
+		{
+			dName: 'Natraj Art Jewellery - Execlusive - 0003',
+			subCat: 'Execlusive',
+			storeLink: 'https://www.amazon.in/Natraj-Art-Designer-Gold-plated-NAJED-0023/dp/B08LBWMCJ9/',
+			image: 'https://i.ibb.co/5YGvQqS/NAJED-0023.jpg',
+		},
+	],
 };
